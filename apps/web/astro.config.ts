08eba0e6a4febb3acdbc43009d91f200c2401ba2
Benjamin Dos Santos <hello@benjamindossantos.be>
2024-02-05 17:13:56 +0100

    feat: configure pandacss

--- apps/web/astro.config.ts
+++ apps/web/astro.config.ts
@@ -1,7 +1,8 @@
 import './env';
 import { defineConfig } from 'astro/config';
 import solid from '@astrojs/solid-js';
+import pandacss from '@pandacss/astro';
 
 export default defineConfig({
-  integrations: [solid()],
+  integrations: [solid(), pandacss()],
 });
